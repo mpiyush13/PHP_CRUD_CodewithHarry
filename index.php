66ae0f51e59288b711b646d6fd0fbce9cdae6312
Piyush Maurya <piyushmaurya@Piyushs-MacBook-Air.local>
2023-08-08 08:00:24 -0700

    Create update delete read operation with database

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+
+// Create table in  connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// // sql to create table
+// $sql = "CREATE TABLE MyGuests (
+// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+// firstname VARCHAR(30) NOT NULL,
+// lastname VARCHAR(30) NOT NULL,
+// email VARCHAR(50),
+// reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+// )";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "Table MyGuests created successfully";
+// } else {
+//   echo "Error creating table: " . $conn->error;
+// }
+
+
+
+
+
+
+
+//Insert into database
+
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// $p='piyushchanda';
+// $q='Maurya';
+// $email='piyushmaurya203@gmail.commmdd';
+// $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+// VALUES ('$p', '$q', '$email1')";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+
+
+
+
+// select data from database
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// $sql = "SELECT id, firstname, lastname FROM MyGuests";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//   // output data of each row
+//   while($row = $result->fetch_assoc()) {
+//     echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//   }
+// } else {
+//   echo "0 results";
+// }
+
+
+
+
+
+//Select data with condition from database
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// $x="Maurya";
+// $sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='$x'";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//   // output data of each row
+//   while($row = $result->fetch_assoc()) {
+//     echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//   }
+// } else {
+//   echo "0 results";
+// }
+
+
+
+
+//Update data from database
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// $w='Piyush';
+// $sql = "UPDATE MyGuests SET lastname='$w' WHERE id=2";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "Record updated successfully";
+// } else {
+//   echo "Error updating record: " . $conn->error;
+// }
+
+
+
+
+//delete data from database
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// // sql to delete a record
+// $sql = "DELETE FROM MyGuests WHERE id=4";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "Record deleted successfully";
+// } else {
+//   echo "Error deleting record: " . $conn->error;
+// }
+
+//  $conn->close();
+
+
+?>
